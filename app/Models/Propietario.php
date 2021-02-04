381a5977f6ee4379c00e042ebc05670dc893365e
Alex20910 <77165096+Alex20910@users.noreply.github.com>
2021-02-04 18:38:11 -0500

    CambioGuards

--- app/Models/Propietario.php
+++ app/Models/Propietario.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Propietario extends Model
+class Propietario extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     /**
      * Le damos a entender a eloquent que el modelo Cultivo corresponde a la tabla create_propietario, 
